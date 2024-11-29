75e90cf28193db9d21ee0303cb886478d4be481d
Caleb Evans <caleb@calebevans.me>
2024-11-28 19:43:53 -0800

    WIP: build out form selector UI

--- scripts/components/Popup.svelte
+++ scripts/components/Popup.svelte
@@ -1,5 +1,23 @@
+<script lang="ts">
+	let formSelector = $state('#app-embed::shadow form');
+
+	async function fillForm(event: SubmitEvent) {
+		event.preventDefault();
+		console.log(`Fill the form at: ${formSelector}`);
+	}
+</script>
+
 <svelte:head>
 	<title>Smart Fake Form Fill</title>
 </svelte:head>
 
 <h1>Smart Fake Form Fill</h1>
+
+<form onsubmit={fillForm}>
+	<p>
+		<label for="formSelector">Form Selector</label>
+		<input id="formSelector" name="formSelector" type="text" bind:value={formSelector} />
+	</p>
+
+	<button>Fill Form</button>
+</form>
