a591d76277a4476a03bf38aac0c2a3655984b176
Caleb Evans <caleb@calebevans.me>
2024-12-13 14:22:21 -0800

    Display options as popup

--- scripts/popup.ts
+++
@@ -1,6 +0,0 @@
-import { mount } from 'svelte';
-import Popup from './components/Popup.svelte';
-
-const popup = mount(Popup, { target: document.getElementsByTagName('main')[0] });
-
-export default popup;
