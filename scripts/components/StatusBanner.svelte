94ebe7ab0d22f7a6012315d77161975e5e3e4f73
Caleb Evans <caleb@calebevans.me>
2024-12-12 16:23:55 -0800

    Fix banner not closing on successful job completion

--- scripts/components/StatusBanner.svelte
+++ scripts/components/StatusBanner.svelte
@@ -4,19 +4,29 @@
 	import LoadingIndicator from './LoadingIndicator.svelte';
 
 	let status: Status | null = $state(null);
+	let justFinishedFillingForm = $state(false);
+	// The number of milliseconds to wait to indicate to the user that the API key
+	// was successfully changed
+	const successDelay = 2000;
 
 	$effect(() => {
 		chrome.runtime.onMessage.addListener((message: StatusUpdateRequest) => {
-			switch (message.action) {
-				case 'updateStatus':
-					status = message.status;
-					break;
+			if (message.action === 'updateStatus') {
+				status = message.status;
 			}
 		});
 	});
+	$effect(() => {
+		if (status?.code === 'SUCCESS') {
+			justFinishedFillingForm = true;
+			setTimeout(() => {
+				justFinishedFillingForm = false;
+			}, successDelay);
+		}
+	});
 </script>
 
-<div class="status-banner" class:visible={status !== null}>
+<div class="status-banner" class:visible={status?.code === 'PROCESSING' || justFinishedFillingForm}>
 	<h1>Smart Fake Form Fill</h1>
 	<div class="status-banner-status">
 		{#if status?.code === 'PROCESSING'}
