e8f915b5f2c5ea89edb8e6d85905013572095043
Caleb Evans <caleb@calebevans.me>
2025-04-23 12:39:46 -0700

    Rename app-icon to extension-icon

--- utilities/generate-icons.sh
+++ utilities/generate-icons.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Generate extension icon
-rsvg-convert -f png -w 256 public/icons/app-icon.svg -o public/icons/app-icon.png
+rsvg-convert -f png -w 256 public/icons/extension-icon.svg -o public/icons/extension-icon.png
 # Generate favicon
-rsvg-convert -f png -w 32 public/icons/app-icon.svg -o public/icons/favicon.png
+rsvg-convert -f png -w 32 public/icons/extension-icon.svg -o public/icons/favicon.png
